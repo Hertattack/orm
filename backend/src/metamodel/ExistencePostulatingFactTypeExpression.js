efa747b5da59701998f83895da9d9ad0ded55a5b
Marcel <marcel.schot@gmail.com>
2020-03-29 19:02:16 +0000

    Some more modelling done.

---
+++ backend/src/metamodel/ExistencePostulatingFactTypeExpression.js
@@ -0,0 +1,14 @@
+'use strict'
+/**
+ * This is a sentence that postulates, states the existence of an object type int he UoD.
+ * 
+ * Not that a object type expression may not occur in this type of expression.
+ */
+
+ import ElementaryFactExpression from './ElementaryFactExpression';
+
+ class ExistencePostulatingFactTypeExpression extends ElementaryFactExpression {
+
+ }
+
+ export default ExistencePostulatingFactTypeExpression;
---
+++ backend/src/metamodel/ObjectExpression.js
@@ -0,0 +1,12 @@
+'use strict'
+/**
+ * The unique identification expression in the UoD for an object type.
+ */
+
+import FactExpression from './FactExpression';
+
+class ObjectExpression extends FactExpression {
+
+}
+
+export default ObjectExpression;
